7fffbaafc792dc376b5aaf72839fbf750b4f559f
himansi1994 <himansipatel1994@gmail.com>
2020-07-10 20:34:40 -0400

    ** Pointcut Expressiond Added **      @execution      @within      @args

--- SpringAOP/src/org/himansi/aop/AopMain.java
+++ SpringAOP/src/org/himansi/aop/AopMain.java
@@ -1,18 +1,20 @@
 package org.himansi.aop;
 
+import org.himansi.aop.model.Circle;
 import org.himansi.aop.service.ShapeService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
-
 public class AopMain {
 
 	public static void main(String[] args) {
-		ApplicationContext  context = new ClassPathXmlApplicationContext("spring.xml");
-		ShapeService shapeService = context.getBean("shapeService",ShapeService.class);    //give bean name and class name so that getbean method automatically takes care of casting it
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//		give bean name and class name so that getbean method automatically takes care of casting it
+		ShapeService shapeService = context.getBean("shapeService", ShapeService.class);
 		System.out.println(shapeService.getCircle().getName());
 //		System.out.println(shapeService.getTriangle().getName());
+		Circle circle = shapeService.getCircle();
+		shapeService.setCircle(circle);
 	}
 
 }
--- SpringAOP/src/org/himansi/aop/aspect/LoggingAspect.java
+++ SpringAOP/src/org/himansi/aop/aspect/LoggingAspect.java
@@ -15,22 +15,46 @@ public class LoggingAspect {
 	 * argument , get*()-only for zero argument "LoggingAdvice()" run before the
 	 * execution of "public String getName()"
 	 */
-	@Before("execution(* get*())")
+	@Before("methodsAcceptingEntities()")
 	public void LoggingAdvice() { // standard format to write an advice
 		System.out.println("Advice run . Get method is called");
 	}
 
-	@Before("execution(* get*())")
-	public void secondAdvice() {
+//	@Before("allGetters()")
+//	public void secondAdvice() {
+//
+//		System.out.println("Second advice executed");
+//	}
 
-		System.out.println("Second advice executed");
-	}
-
-//	ponintcut defines dummy method to hold an expression "execution(* get*())" so that we can replace it by "allGetters()" for ex -@Before("allGetters()")
+	/*
+	 * ->can use one pointcut applies to different method ->ponintcut defines dummy
+	 * method to hold an expression "execution(* get*())" so that we can replace it
+	 * by "allGetters()" for ex -@Before("allGetters()")
+	 */
 	@Pointcut("execution(* get*())")
 	public void allGetters() {
 	}
 
+	/*
+	 * org.himansi.aop.model..* --> all the classes in the package( model ) and sub
+	 * packages as well org.himansi.aop.model.Circle.*(..) or
+	 * org.himansi.aop.model.Circle --> all the method of circle
+	 * org.himansi.aop.model.* -->all the classes of model package
+	 */
+	@Pointcut("within(org.himansi.aop.model.Circle)")
+	public void allCircleMethods() {
+	}
+
+	/*
+	 * @args This PCD limits matching to join points where the runtime type of the
+	 * actual arguments passed have annotations of the given type(s). Suppose that
+	 * we want to trace all the methods accepting beans annotated with @Entity
+	 * annotation:
+	 */
+	@Pointcut("args(org.himansi.aop.model.Circle)")
+	public void methodsAcceptingEntities() {
+	}
+	
 	/*
 	 * @Before("execution(public String getName())") // "LoggingAdvice()" run before
 	 * the execution of "public String // getName()" public void LoggingAdvice() {
